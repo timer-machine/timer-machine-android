dfa35563dc8bc9bc080e7ac82e4a5d8be4306a9b
DeweyReed <32358486+DeweyReed@users.noreply.github.com>
2022-08-22 22:16:31 +0800

    Lock the logging

--- app-base/src/main/java/xyz/aprildown/timer/app/base/utils/LogToFileTree.kt
+++ app-base/src/main/java/xyz/aprildown/timer/app/base/utils/LogToFileTree.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import timber.log.Timber
 import xyz.aprildown.timer.domain.utils.Constants
 import java.io.File
@@ -12,37 +14,49 @@ import java.util.Locale
 
 class LogToFileTree(private val context: Context) : Timber.Tree() {
 
+    private val logMutex = Mutex()
+    private val cachedMessage = mutableListOf<String>()
+    private val messageLock = Any()
+
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+        synchronized(messageLock) {
+            cachedMessage += message
+        }
+
         GlobalScope.launch(Dispatchers.IO) {
-            try {
-                val logFile = File(context.filesDir, Constants.FILENAME_RUNNING_LOG)
+            logMutex.withLock {
+                try {
+                    val messageToLog = synchronized(messageLock) {
+                        if (cachedMessage.isEmpty()) return@withLock
+                        cachedMessage.removeAt(0)
+                    }
 
-                when {
-                    !logFile.exists() -> {
-                        logFile.createNewFile()
+                    val logFile = File(context.filesDir, Constants.FILENAME_RUNNING_LOG)
+
+                    when {
+                        !logFile.exists() -> {
+                            logFile.createNewFile()
+                        }
+                        logFile.length() > 10 /* MB */ * 1024 /* KB */ * 1024L /* B */ -> {
+                            logFile.delete()
+                            logFile.createNewFile()
+                        }
                     }
-                    logFile.length() > 10 /* MB */ * 1024 /* KB */ * 1024L /* B */ -> {
-                        logFile.delete()
-                        logFile.createNewFile()
-                    }
+
+                    logFile.appendText(
+                        buildString {
+                            append(
+                                SimpleDateFormat("yyyy-MM-dd-kk-mm-ss", Locale.getDefault())
+                                    .format(System.currentTimeMillis())
+                            )
+                            append(" ")
+                            append(if (tag.isNullOrBlank()) "" else "$tag ")
+                            append(messageToLog)
+                            append("\n")
+                        }
+                    )
+                } catch (_: Exception) {
                 }
-
-                logFile.appendText(
-                    buildString {
-                        append(
-                            SimpleDateFormat("yyyy-MM-dd-kk-mm-ss", Locale.getDefault())
-                                .format(System.currentTimeMillis())
-                        )
-                        append(" ")
-                        append(priority)
-                        append(" ")
-                        append(if (tag.isNullOrBlank()) "" else "$tag ")
-                        append(message)
-                        append("\n")
-                    }
-                )
-            } catch (e: Exception) {
-                e.printStackTrace()
             }
         }
     }
