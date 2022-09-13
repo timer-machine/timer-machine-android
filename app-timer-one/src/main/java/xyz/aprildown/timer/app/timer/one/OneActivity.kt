0b9e1874bae21288653da57d1a642aec9ab8d52c
DeweyReed <32358486+DeweyReed@users.noreply.github.com>
2022-09-13 18:53:00 +0800

    Correct implementation in OneActivity.onSupportNavigateUp()

--- app-timer-one/src/main/java/xyz/aprildown/timer/app/timer/one/OneActivity.kt
+++ app-timer-one/src/main/java/xyz/aprildown/timer/app/timer/one/OneActivity.kt
@@ -6,13 +6,13 @@ import android.content.res.Configuration
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.appcompat.widget.Toolbar
 import androidx.core.app.ActivityCompat
 import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
 import xyz.aprildown.timer.app.base.data.PreferenceData.storedAudioTypeValue
 import xyz.aprildown.timer.app.base.ui.BaseActivity
 import xyz.aprildown.timer.app.base.utils.ScreenWakeLock
+import xyz.aprildown.timer.app.timer.one.databinding.ActivityOneBinding
 import xyz.aprildown.timer.domain.entities.TimerEntity
 import xyz.aprildown.timer.domain.utils.Constants
 import xyz.aprildown.timer.presentation.one.OneViewModel
@@ -27,11 +27,13 @@ class OneActivity : BaseActivity(), OneActivityInterface {
 
     private val viewModel: OneViewModel by viewModels()
 
+    private lateinit var binding: ActivityOneBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_one)
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
+        binding = ActivityOneBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
         supportActionBar?.run {
             setDisplayHomeAsUpEnabled(true)
             setDisplayShowTitleEnabled(false)
@@ -48,16 +50,14 @@ class OneActivity : BaseActivity(), OneActivityInterface {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController =
-            (supportFragmentManager.findFragmentById(R.id.fragmentOneHost) as NavHostFragment).navController
-        val currentId = navController.currentDestination?.id
-        return if (currentId == R.id.fragmentOne) {
-            finish()
-            true
-        } else {
-            navController.navigateUp()
-            false
+        if (binding.fragmentOneHost.getFragment<NavHostFragment>().navController.navigateUp()) {
+            return false
         }
+        if (!isTaskRoot) {
+            onBackPressed()
+            return true
+        }
+        return super.onSupportNavigateUp()
     }
 
     override fun onResume() {
